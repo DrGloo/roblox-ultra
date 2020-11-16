b84cb2df8507e2e4262f9d4e6ee05e590e868ee0
Raishan Bernard <raishanbernard@outlook.com>
2020-11-15 22:08:15 -0500

    Framework update

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
 // Future JavaScript will go here
+
+//var divs = document.getElementsByTagName("div");
+//for(var i=0,div;div =divs[i];i++){
+ //   if(div.className.match('content')){
+       
+ //   }
+//}
+
+document.getElementsByClassName('container-fluid')[0].style.backgroundColor = rgb(255, 9, 9);
